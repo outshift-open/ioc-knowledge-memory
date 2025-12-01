8fe0f2d9918b493d792a2947935ddfb5a26cd0dd
Sushama Shroff <sushroff@cisco.com>
2025-12-01 13:21:11 -0800

    update lint script flake configs

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -5,6 +5,6 @@ echo "Running Black formatter check..."
 poetry run black --check src/
 
 echo "Running Flake8 linter..."
-poetry run flake8 src/ --max-line-length=120 --extend-ignore=E203,W503
+poetry run flake8 src/ --max-line-length=120 --extend-ignore=E203,W503,W293,F401
 
 echo "Linting DONE"
